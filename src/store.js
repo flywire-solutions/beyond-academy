b3bb669efe5d87feeedf819e8fbfbc33efe5acb0
George Goodchild <georgegoodchild@gmail.com>
2020-10-21 18:57:30 +0100

    initial commit

---
+++ src/store.js
@@ -0,0 +1,206 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import dataService from "./api/flywireApi";
+import utils from "./utils";
+
+Vue.use(Vuex);
+
+const initialState = {
+  ui: {
+    isLoading: false,
+    title: null,
+    subTitle: null,
+    configErrors: [],
+    paymentErrors: [],
+  },
+  portal: {
+    env: null,
+    portalCode: null,
+    recipient: {},
+  },
+  payment: {
+    amount: null,
+    firstName: null,
+    lastName: null,
+    email: null,
+    phone: null,
+    address: null,
+    city: null,
+    country: null,
+    callbackUrl: null,
+    callbackId: null,
+    parameters: {},
+  },
+  result: {
+    status: null,
+    reference: null,
+    paymentMethod: null,
+  },
+};
+
+const getters = {
+  formattedAmount: (state) => {
+    return utils.formatAmount(
+      state.payment.amount,
+      state.portal.recipient.currency
+    );
+  },
+  client: (state) => {
+    return {
+      name: state.portal.recipient.name,
+      logo: state.portal.recipient.logo_url,
+    };
+  },
+  canPay: (state) => {
+    return !state.ui.isLoading && state.ui.configErrors.length === 0;
+  },
+  isError: (state) => {
+    return (
+      !state.ui.isLoading &&
+      state.result.status !== "success" &&
+      state.result.status !== "pending"
+    );
+  },
+};
+
+const mutations = {
+  UI_START_LOADING(state) {
+    state.ui.isLoading = true;
+  },
+  UI_STOP_LOADING(state) {
+    state.ui.isLoading = false;
+  },
+  UI_INIT(state, payload) {
+    (state.ui.title = payload.title), (state.ui.subTitle = payload.subTitle);
+  },
+  UI_ADD_CONFIG_ERROR(state, payload) {
+    state.ui.configErrors.push(payload.error);
+  },
+  UI_ADD_PAYMENT_ERRORS(state, payload) {
+    state.ui.paymentErrors = payload.errors;
+  },
+  UI_CLEAR_ERRORS(state) {
+    state.ui.configErrors = [];
+    state.ui.paymentErrors = [];
+  },
+  PAYMENT_INIT(state, payload) {
+    state.payment.amount = payload.amount;
+    state.payment.firstName = payload.firstName;
+    state.payment.lastName = payload.lastName;
+    state.payment.email = payload.email;
+    state.payment.phone = payload.phone;
+    state.payment.address = payload.address;
+    state.payment.city = payload.city;
+    state.payment.country = payload.country;
+    state.payment.callbackUrl = payload.callbackUrl;
+    state.payment.callbackId = payload.callbackId;
+    state.payment.parameters =
+      {
+        payment_number: payload.num,
+        zoho_entity_type: payload.type,
+        zoho_entity_id: payload.id,
+        invoice_number: `${payload.firstName} ${payload.lastName}`
+      };
+  },
+  PORTAL_INIT(state, payload) {
+    state.portal.env = payload.env;
+    state.portal.portalCode = payload.portalCode;
+    state.portal.recipient = payload.recipient;
+  },
+  RESULT_INIT(state, payload) {
+    (state.result.status = payload.status),
+      (state.result.reference = payload.reference),
+      (state.result.paymentMethod = payload.paymentMethod);
+  },
+};
+
+const actions = {
+  load: ({ commit }) => {
+    commit("UI_START_LOADING");
+    let { amount, ccy, firstName, lastName, email, phone, address, city, country, env = "prod", id, num, title } = utils.getQueryStringValues();
+
+    function addConfigErrorIf(fn, message) {
+      if (fn()) {
+        commit("UI_ADD_CONFIG_ERROR", { error: message });
+      }
+    }
+
+    addConfigErrorIf(() => !ccy, "Currency code not supplied");
+    addConfigErrorIf(() => ccy && (ccy.length != 3), `Invalid currency code supplied (${ccy})`);
+    addConfigErrorIf(() => !id, "Zoho CRM entity id not supplied");
+    addConfigErrorIf(() => !num, "Payment number not supplied");
+    addConfigErrorIf(() => num && (isNaN(num) || parseInt(num) < 1 || parseInt(num) > 1), `Invalid payment number (${num})`);
+    addConfigErrorIf(() => !amount, "Amount not supplied");
+    addConfigErrorIf(() => amount && (isNaN(amount) || parseFloat(amount) <= 0), `Invalid amount (${amount})`);
+
+    let type = "";
+    let callbackUrl = "";
+    let callbackId = id;
+
+    if (parseInt(num) === 1) {
+      type = "Lead";
+      callbackUrl = "https://flow.zoho.com/699449867/flow/webhook/incoming?zapikey=1001.4d28aad7882cd97478c0156d4998784d.afea37c18f1a9f73284b8eb6b80b444c&isdebug=false";
+    } else if (parseInt(num) === 2) {
+      type = "Contact";
+      callbackUrl = "https://flow.zoho.com/699449867/flow/webhook/incoming?zapikey=1001.bb385eeeef5bb91d183c1c5e7653ca44.52199737f38cd0523b96c604b84a4a4d&isdebug=false";
+    } else if (parseInt(num) === 3) {
+      type = "Contact";
+      callbackUrl = "https://flow.zoho.com/699449867/flow/webhook/incoming?zapikey=1001.3bf65dc00dbb0c2b545f656346b4d220.b86c730ad18441b545fbf158b4cbeb79&isdebug=false";
+    }
+
+    const portalCodes = {
+      AUD: "BYA",
+      EUR: "BDE",
+      GBP: "BYG",
+      JPY: "BAJ",
+      SEK: "BAS",
+      USD: "BAU"
+    }
+    let portalCode = portalCodes[ccy];
+
+    commit("PAYMENT_INIT", { amount: parseFloat(amount), firstName, lastName, email, phone, address, city, country, callbackUrl, callbackId, type, id, num });
+
+    title = title ?? "Beyond Academy";
+    let subTitle = `Payment Num: ${num}`;
+    commit("UI_INIT", { title, subTitle });
+
+    const recipientPromise = dataService.getRecipient(portalCode, env);
+
+    Promise.all([recipientPromise]).then((values) => {
+      addConfigErrorIf(() => !values[0].id, `Client not found (${portalCode})`);
+
+      commit("PORTAL_INIT", { env, portalCode, recipient: values[0] });
+      commit("UI_STOP_LOADING");
+    });
+  },
+  paymentSetErrors: ({ commit }, value) => {
+    commit("UI_ADD_PAYMENT_ERRORS", {
+      errors: value,
+    });
+  },
+  pay: ({ commit }) => {
+    commit("UI_CLEAR_ERRORS");
+  },
+  complete: ({ commit }, value) => {
+    let { reference, status, payment_method: paymentMethod } = value;
+
+    commit("RESULT_INIT", {
+      status,
+      reference,
+      paymentMethod,
+    });
+  },
+};
+
+const debug = process.env.NODE_ENV !== "production";
+
+const store = new Vuex.Store({
+  strict: debug,
+  state: initialState,
+  getters,
+  mutations,
+  actions,
+});
+
+export default store;
---
+++ src/views/QuickPay.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="quick-pay">
+        <div class="card text-center">
+            <div class="card-header">
+                <img src="@/assets/flywire.png" height="20px"/>
+            </div>
+            <div class="card-body">
+                <img v-if="client.logo" :src="client.logo" :alt="client.name" />
+                <h4>{{ ui.title }}
+                  <small class="text-muted">{{ ui.subTitle }}</small>
+                </h4>
+                <div v-if="formattedAmount" class="payment-info">
+                    <h3>
+                      <small>Please pay</small>
+                      {{ formattedAmount }}
+                    </h3>
+                </div>
+                <div v-if="ui.configErrors.length > 0 || ui.paymentErrors.length > 0" class="errors">
+                    <template v-for="e in ui.configErrors">
+                        <span :key="e">{{ e }}</span>
+                    </template>
+                    <template v-for="e in ui.paymentErrors">
+                        <span :key="e.id">{{ e.msg }}</span>
+                    </template>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button :disabled="!canPay" class="btn btn-primary" v-on:click="onPay">Make Payment</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { mapGetters, mapState } from 'vuex'
+
+export default {
+    name: 'quick-pay',
+    computed: {
+        ...mapState({
+          ui: 'ui',
+          portal: 'portal',
+          payment: 'payment'
+        }),
+        ...mapGetters([
+          'formattedAmount',
+          'client',
+          'canPay'
+       ])
+    },
+    mounted: function() {
+      this.$store.dispatch('load');
+    },
+    methods: {
+      onPay: function() {
+        var that = this;
+        var popup = null;
+
+        this.$store.dispatch('pay')
+
+        const config = {
+            env: this.portal.env,
+            recipientCode: this.portal.portalCode,
+
+            amount: this.payment.amount,
+            recipientFields: this.payment.parameters,
+
+            requestPayerInfo: true,
+            firstName: this.payment.firstName,
+            lastName: this.payment.lastName,
+            email: this.payment.email,
+            phone: this.payment.phone,
+            address: this.payment.address,
+            city: this.payment.city,
+            country: this.payment.country,
+
+            callbackId: this.payment.callbackId,
+            callbackUrl: this.payment.callbackUrl,
+
+            nonce: this.payment.callbackId,
+
+            onInvalidInput: (errs) => {
+                that.$store.dispatch('paymentSetErrors', errs)
+            },
+            onCompleteCallback: (args) => {
+                // eslint-disable-next-line no-console
+                console.log(args);
+                that.$store.dispatch('complete', args);
+                that.$router.push('complete');
+            }
+        }
+
+        popup = window.FlywirePayment.initiate(config);
+        popup.render();
+      }
+    }
+}
+</script>
+
+<style lang="scss">
+    .quick-pay {
+        width: 400px;
+        margin: 50px auto;
+
+        .card-body {
+            padding-bottom: 0;
+        }
+    }
+    h4, h3 {
+        small {
+            display: block;
+            font-size: 1rem;
+        }
+    }
+    .payment-info, .form-group { 
+        margin-top: 8px;
+    }
+
+    .card-body {
+        margin-bottom: 10px;
+    }
+
+    .payment-info{ 
+        background-color: #1274C4;
+        color: white;
+        margin: 0 -20px;
+        padding: 5px 0;
+    }
+
+    .errors {
+        background-color: #D3556B;
+        color: white;
+        margin: 5px -20px;
+        padding: 5px 0;
+
+        span {
+            display: block;
+        }
+    }
+
+    
+</style>
+
