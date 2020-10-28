221219e58702a28c0dd70f7528128c1e5cf2a8bc
George Goodchild <georgegoodchild@gmail.com>
2020-10-28 09:34:16 +0000

    payment number

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 /*
  * Use this one when building for GitHub Pages
 */
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/beyond-academy/'
-//     : '/',
-//   outputDir: 'docs'
-// }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/beyond-academy/'
+    : '/',
+  outputDir: 'docs'
+}
 
 /*
  * Use this one when building for Flywire hosting
 */
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? ''
-    : '/',
-  outputDir: 'flywire-output'
-}
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? ''
+//     : '/',
+//   outputDir: 'flywire-output'
+// }
